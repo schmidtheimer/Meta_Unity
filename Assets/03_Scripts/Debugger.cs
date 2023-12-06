4453a1ad1085d8baeac8383350cfd63467d0eb13
CUTE Desktop <cutecenter01@gmail.com>
2023-12-06 14:35:51 +0800

    Added more poses

--- Assets/03_Scripts/Debugger.cs
+++ Assets/03_Scripts/Debugger.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Debugger : MonoBehaviour{
-    
-    public void PrintLog(string message) => Debug.Log(message);
-    public void PrintErrorLog(string message) => Debug.Log(message);
+
+    public void PrintLog(string message) { Debug.Log(message); }
+    public void PrintErrorLog(string message) { Debug.LogError(message); }
 
 
 }
